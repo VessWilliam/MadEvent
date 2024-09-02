05a3e41c2cea1bff7798649751789d71e0b9dd6e
vesswilliam <vesswilliam1994@gmail.com>
2024-09-02 09:20:58 +0800

    modify

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -13,16 +13,20 @@ const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
+//Cors
 app.use((0, cors_1.default)({
     origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
+//JWT
 app.use(authMiddle_1.auth);
+//Json
 app.use(express_1.default.json());
-// User routes
+//routes
 app.use("/api/users", userRoutes_1.default);
 app.use("/api/event", eventRoutes_1.default);
+//Mongoose ODM
 mongoose_1.default
     .connect(process.env.MONGO_URI)
     .then(() => {
